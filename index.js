d0a9648d7429a138992fe14265d776178ac92b2e
Mai Vang <vmaineng@gmail.com>
2022-04-26 18:32:02 -0700

    commit tree 7d40843cbb1daaaa8aee29c4e1bc1b739716bc71

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Dude",
+    streetAddress: "123 Elm",
+    }
+    
+    function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+    }
+    
+    function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value;
+    return employee;
+    }
+    
+    function deleteFromEmployeeByKey(employee, name){
+    const newEmployee = {...employee};
+    delete newEmployee[name];
+    return newEmployee;
+    };
+    
+    function destructivelyDeleteFromEmployeeByKey (employee, name) {
+    delete employee.name;
+    return employee;
+    };
